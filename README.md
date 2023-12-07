0f9d4bc82989d39d4c15b353e85d5fc82affcf1c
Younes Aassila <47226184+younesaassila@users.noreply.github.com>
2023-12-07 14:24:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# start-menu-sorter
+
+Sorts the Windows start menu
+
+## Install
+
+```sh
+python .\setup.py install
+```
+
+## Usage
+
+```sh
+start-menu-sorter [--clean] [--dry-run]
+```
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="start-menu-sorter",
+    version="1.0.0",
+    description="Sorts the Windows start menu",
+    url="https://github.com/younesaassila/start-menu-sorter",
+    author="Younes Aassila",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.7, <4",
+    install_requires=["click"],
+    entry_points={
+        "console_scripts": [
+            "start-menu-sorter=start_menu_sorter:main",
+        ],
+    },
+)
